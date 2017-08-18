8d4ff6f833fc978f287ea276c7a02805ee7bcf77
Riko Laimena <30501260+BetterBitter@users.noreply.github.com>
2017-08-18 22:55:01 +1000

    Add files via upload

--- Assignment1/ViewController.swift
+++ Assignment1/ViewController.swift
@@ -8,18 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var banks = ["bank1","bank2","bank3"]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return banks.count
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = banks[indexPath.row]
+        
+        return cell
     }
 
 
 }
-
