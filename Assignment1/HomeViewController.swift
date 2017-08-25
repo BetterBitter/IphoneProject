5fad87de7f9641c81d01c934bfb225604f09594d
Riko Laimena <30501260+BetterBitter@users.noreply.github.com>
2017-08-25 21:24:46 +1000

    Add files via upload

---
+++ Assignment1/HomeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tarot
+//
+//  Created by Rodney Cocker on 20/03/2016.
+//  Copyright © 2016 RMIT. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController
+{
+    // Lifecycle method for performing tasks after the view has loaded
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    // Lifecycle method for clearing up memory resources
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
---
+++ Assignment1/MapSatelliteViewController.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class MapSatelliteViewController: UIViewController
+{
+    // Lifecycle method for performing tasks after the view has loaded
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    // Lifecycle method for clearing up memory resources
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
